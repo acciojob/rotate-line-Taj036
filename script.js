a496d774ea1911ba17a695020d47a3071173a1af
Taj036 <121137573+Taj036@users.noreply.github.com>
2024-01-06 17:31:00 +0000

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let line  =  document.getElementById('line');
+let rotateAngle = 0 ; 
+function rotateLine(){
+	 rotateAngle+= 2 ;
+	line.style.transform = `rotate(${rotateAngle}deg)`;
+	requestAnimationFrame(rotateLine);
+}
+rotateLine()
